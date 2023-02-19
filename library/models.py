683c9668b79e210a79af24fe61285f6d423c05d4
emmlev0809 <119499443+emmlev0809@users.noreply.github.com>
2023-02-19 23:42:29 +0000

    Update models.py

--- library/models.py
+++ library/models.py
@@ -1,21 +1,65 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, UniqueConstraint, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 # Base is called an Abstract Base Class - our SQL Alchemy models will inherit from this class
 Base = declarative_base()
 
+# Sets up a link table with activity_id and person_id as foreign keys
+# Base.metadata is a container object that keeps together many different features of the database
+book_author = Table('book_author',
+                        Base.metadata,
+                        Column('id', Integer, primary_key=True),
+                        Column('author_id', ForeignKey('author.id')),
+                        Column('publisher_id', ForeignKey('publisher.id')),
+                        UniqueConstraint('author_id', 'publisher_id')
+                        )
+
+
+# Sets up an Activity table, this references "attendees" via the person_activities table
+class Book(Base):
+    __tablename__ = 'book'
+    book_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    ISBN_number = Column(Integer, unique=True, nullable=False)
+    num_pages = Column(Integer, nullable=False)
+    publication_date = Column(Integer, nullable=False)
+    publication_id = Column(Integer, unique=True, nullable=False)
+
+    author = relationship("Author",
+                             secondary=book_author,
+                             order_by='(Author.author_name)',
+                             back_populates="author")
+
+    # Gives a representation of an Activity (for printing out)
+    def __repr__(self):
+        return f"<Book({self.name})>"
+
 
 # Sets up a Person table, this references "activities" via the person_activities table
-class Person(Base):
-    __tablename__ = 'person'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
+class Author(Base):
+    __tablename__ = 'author'
+    author_id = Column(Integer, primary_key=True, autoincrement=True)
+    author_name = Column(String, nullable=False)
+    book = relationship("Book",
+                              secondary=book_author,
+                              order_by='Book.book_name',
+                              back_populates="book")
 
     # Gives a representation of a Person (for printing out)
     def __repr__(self):
-        return f"<Person({self.first_name} {self.last_name})>"
+        return f"<Author({self.author_name})>"
 
-    # Include a method:
-    def greeting(self):
-        print(f'{self.first_name} says "hello"!')
+
+
+
+# Sets up a Location table, this references "activities" via the person_activities table
+class Publisher(Base):
+    __tablename__ = 'publisher'
+    publisher_id = Column(Integer, primary_key=True, autoincrement=True)
+    publisher_name = Column(String, nullable=False)
+
+
+    # Gives a representation of a Person (for printing out)
+    def __repr__(self):
+        return f"<Publisher({self.publisher_name})>"
